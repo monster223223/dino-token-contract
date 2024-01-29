379977e37349ebb66a40acb481f22f68967189b2
monster223223 <richdev0223@gmail.com>
2024-01-29 15:19:30 +0100

    fixed minor issue

--- DinoSwap_ERC20.sol
+++ DinoSwap_ERC20.sol
@@ -1,15 +1,6 @@
 
 pragma solidity ^0.8.0;
 
-/******************************************/
-/*           IERC20 starts here           */
-/******************************************/
-
-// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
-
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP.
- */
 interface IERC20 {
     /**
      * @dev Returns the amount of tokens in existence.
